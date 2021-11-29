3454d91c22e70e143aadaeb97a69693b8475e52d
Mattias Kind <mattias.kind@gmail.com>
2021-11-29 13:12:08 +0100

    Läs fil med async/await och svara  med json vid begäran

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const express = require('express');
-const app = express();
+const util = require('util');
 
+const app = express();
 const port = 3000;
 
-const readFilePromise = (file) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf-8', (err, data) => {
-      if (err) reject('Kan inte hitta filen');
-      resolve(data);
-    });
-  });
+// const readFilePromise = (file) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(file, 'utf-8', (err, data) => {
+//       if (err) reject('Filen kunde inte hittas');
+//       resolve(data);
+//     });
+//   });
+// };
+
+const getPostsData = async (path) => {
+  try {
+    return await fs.readFile(path, 'utf-8');
+  } catch (error) {
+    throw 'Filen kunde inte hittas!';
+  }
 };
 
-// (async () => {
-//   const data = await readFilePromise('data/example-data.json');
-//   console.log(data);
-// })();
+// const writePost = () => {
+//   try {
+//     return fs.writeFile('data/example-data.json');
+//   } catch (error) {}
+// };
 
-readFilePromise('data/example-data.json').then((data) => {
-  console.log(data);
+app.get('/posts', (req, res) => {
+  console.log('Inkommande begäran');
+  (async () => {
+    try {
+      const posts = await getPostsData('data/example-data.json');
+      res.json(JSON.parse(posts));
+    } catch (error) {
+      res.send(error);
+    }
+  })();
 });
 
-// app.get('/', (req, res) => {
-//   console.log('incoming request');
-// });
+app.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  (async () => {
+    let posts = await getPostsData();
+    posts = JSON.parse(posts);
+    const post = posts.find((post) => post.id === Number(id));
+    res.json(post);
+  })();
+});
 
-// app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
