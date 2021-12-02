465bf47c1f91a7fe3e1750fc5ef56c64b4061b71
Mattias Kind <mattias.kind@gmail.com>
2021-12-02 09:13:53 +0100

    Ytterligare kod för att kunna validera på klientsidan

--- public/js/script.js
+++ public/js/script.js
@@ -1,54 +1,70 @@
-window.addEventListener(
-  'load',
-  () => {
-    //const formBtn = document.querySelector('input[type="submit"');
-    const form = document.querySelector('form');
-    const inputFields = document.querySelectorAll('input:not([type="reset"]):not([type="submit"])');
-    const textArea = document.querySelector('textarea');
-    const btnSend = document.querySelector('input[type="submit"]');
-    const requiredFields = document.querySelectorAll('.required');
+window.addEventListener('load', () => {
+  //const formBtn = document.querySelector('input[type="submit"');
+  const form = document.querySelector('form');
+  const inputFields = document.querySelectorAll('input:not([type="reset"]):not([type="submit"])');
+  const textArea = document.querySelector('textarea');
+  const btnSend = document.querySelector('input[type="submit"]');
+  const requiredFields = document.querySelectorAll('.required');
+  const submitMsg = document.querySelector('.submit-msg');
 
-    window.formFieldErrors = [];
+  window.formFieldErrors = [];
 
-    btnSend.disabled = true;
-    for (let i = 0; i < requiredFields.length; i++) {
-      requiredFields[i].addEventListener('input', function () {
-        btnSend.disabled = false;
-        for (let j = 0; j < requiredFields.length; j++) {
-          if (!requiredFields[j].value) btnSend.disabled = true;
-        }
-      });
-    }
+  btnSend.disabled = true;
+  for (let i = 0; i < requiredFields.length; i++) {
+    requiredFields[i].addEventListener('input', function () {
+      btnSend.disabled = false;
+      for (let j = 0; j < requiredFields.length; j++) {
+        if (!requiredFields[j].value) btnSend.disabled = true;
+      }
+    });
+  }
 
-    form.addEventListener('submit', (e) => {
-      console.log('form submit');
+  form.addEventListener(
+    'submit',
+    (e) => {
+      if (formFieldErrors.length > 0) {
+        formFieldErrors.forEach((id) => {
+          document.querySelector(`label[for="${id}"]`).children[0].classList.add('hidden');
+        });
+      }
+      formFieldErrors = [];
+      submitMsg.classList.add('hidden');
 
       inputFields.forEach((element) => {
         if (element.id === 'author') validate(element, isValidInput);
         if (element.id === 'email') validate(element, isValidEmail);
       });
 
-      validate(textArea, isValidInput);
+      validate(textArea, isValidTextArea);
 
       if (formFieldErrors.length > 0) {
-        console.log('errors found');
         e.preventDefault();
+        formFieldErrors.forEach((id) => {
+          document.querySelector(`label[for="${id}"]`).children[0].classList.remove('hidden');
+        });
+      } else {
+        submitMsg.classList.remove('hidden');
       }
-    });
+    },
+    true
+  );
 
-    function validate(element, callback) {
-      if (!callback(element.value)) formFieldErrors.push(element.id);
-    }
+  function validate(element, callback) {
+    if (!callback(element.value)) formFieldErrors.push(element.id);
+  }
 
-    function isValidInput(input) {
-      const regex = /^[a-ö, -]{2,}$/i;
-      return regex.test(input);
-    }
+  function isValidTextArea(input) {
+    const regex = /^[\w\d\s]+$/i;
+    return regex.test(input);
+  }
 
-    function isValidEmail(input) {
-      const regex = /^[a-z0-9]+\.*[a-z0-9]*@[a-z0-9]+\.[a-z]{2,4}$/i;
-      return regex.test(input);
-    }
-  },
-  true
-);
+  function isValidInput(input) {
+    const regex = /^[a-ö, -]{2,}$/i;
+    return regex.test(input);
+  }
+
+  function isValidEmail(input) {
+    const regex = /^[a-z0-9]+\.*[a-z0-9]*@[a-z0-9]+\.[a-z]{2,4}$/i;
+    return regex.test(input);
+  }
+});
